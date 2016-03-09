b19ac6e3cbe0de1214172a697d299ca3edf1e67f
Alexander Trost <galexrt@googlemail.com>
2016-03-09 10:09:30 +0100

    Added wordpress example

--- tasks/runit101/wordpress.sh
+++ tasks/runit101/wordpress.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+docker run \
+    --name wordpress \
+    -d \
+    --link mysql:database \
+    -p 80:80 \
+    -e 'WORDPRESS_DB_NAME=wordpress' \
+    -e 'WORDPRESS_DB_USER=wordpress' \
+    -e 'WORDPRESS_DB_PASSWORD=wordpress' \
+    -e 'WORDPRESS_AUTH_KEY=SECURE_AUTH_KEY' \
+    -e 'WORDPRESS_LOGGED_IN_KEY=SECURE_LOGGED_IN_KEY' \
+    -e 'WORDPRESS_AUTH_SALT=SECURE_AUTH_SALT' \
+    -e 'WORDPRESS_LOGGED_IN_SALT=SECURE_LOGGED_IN_SALT' \
+    wordpress
