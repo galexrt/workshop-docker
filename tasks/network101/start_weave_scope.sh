1245b7dec8ac40c6521acb7f79e89ead7ed0f634
Alexander Trost <galexrt@googlemail.com>
2016-03-09 10:39:05 +0100

    Added notice to open weave scope page in browser

--- tasks/network101/start_weave_scope.sh
+++ tasks/network101/start_weave_scope.sh
@@ -3,3 +3,5 @@
 sudo wget -O /usr/local/bin/scope https://git.io/scope
 sudo chmod a+x /usr/local/bin/scope
 sudo scope launch
+
+echo "Now open http://127.0.0.1:4040 in your web browser, to see the Weave Scope map."
