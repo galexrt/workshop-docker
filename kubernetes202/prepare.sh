0ffb806a048c338eec491a65dcdb3cb3e2428a54
Alexander Trost <galexrt@googlemail.com>
2016-06-24 15:10:54 +0200

    Renamed ansible to ansible_old Small changes to the prepare.sh

--- kubernetes202/prepare.sh
+++ kubernetes202/prepare.sh
@@ -2,11 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-KUBERNETES_VERSION="1.2.2"
+KUBERNETES_VERSION="v1.3.0-beta.2"
 
 cd "$DIR" || exit 1
 
-wget "https://github.com/kubernetes/kubernetes/releases/download/v$KUBERNETES_VERSION/kubernetes.tar.gz" -O "$DIR/kubernetes.tar.gz"
+wget "https://github.com/kubernetes/kubernetes/releases/download/$KUBERNETES_VERSION/kubernetes.tar.gz" -O "$DIR/kubernetes.tar.gz"
 tar xfz "$DIR/kubernetes.tar.gz"
 rm -rf "$DIR/kubernetes/contrib"
 git clone https://github.com/kubernetes/contrib.git "$DIR/kubernetes/contrib/"
@@ -21,7 +21,7 @@ cp -r "$DIR/kubernetes/server/kubernetes/saltbase" "$DIR/kubernetes/contrib"
 
 mkdir -p "$DIR/kubernetes/_output/local/go"
 cp -r "$DIR/kubernetes/server/kubernetes/server/bin" "$DIR/kubernetes/_output/local/go"
-cp -r "$DIR/ansible" "$DIR/kubernetes/contrib"
+cp -r "$DIR/ansible" "$DIR/kubernetes/contrib/"
 
 echo "++++++++++++++++++++++++++++++++++"
 echo "=> Files prepared. Done."
